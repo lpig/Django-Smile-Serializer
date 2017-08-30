b17368f05927a5a4a7dd3c1488f82b9e1175570b
lpig <o55662000@yeah.net>
2017-08-30 11:33:55 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+
+url = "https://github.com/lpig/Django-Smile-Serializer"
+
+long_description = "Django-Smile-Serializer"
+
+setup(name="Django-Smile-Serializer",
+      version=VERSION,
+      description=long_description,
+      maintainer="l-pig",
+      maintainer_email="o55662000@yeah.net",
+      url=url,
+      long_description=long_description,
+      install_requires=[
+          'arrow',
+      ],
+      packages=find_packages('.'),
+      )
